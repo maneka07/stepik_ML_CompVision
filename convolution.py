2ab712e5bdaa55a7474675e58b48b790131685c8
Maneka07 <maneka07@yandex.ru>
2021-12-10 16:18:47 +0300

    Imlemented 1) Function that adds zero padding to images 2) Function that calculates the shape of the output tensor
    Added template for the Convolution layer implementation from the course

--- convolution.py
+++ convolution.py
@@ -6,6 +6,7 @@ Created on Fri Nov 19 17:14:55 2021
 """
 
 import torch
+from abc import ABC, abstractmethod
 # Создаем входной массив из двух изображений RGB 3*3
 input_images = torch.tensor(
       [[[[0,  1,  2],
@@ -71,8 +72,16 @@ correct_padded_images = torch.tensor(
           [0., 51., 52., 53.,  0.],
           [0.,  0.,  0.,  0.,  0.]]]])
 
+#Add zero padding to images
 def get_padding2d(input_images):
-    padded_images = #TODO         
+    nimgs = input_images.shape[0]
+    nchans = input_images.shape[1]
+    nrows = input_images.shape[2]
+    ncols = input_images.shape[3]
+    padded_images = torch.zeros((nimgs, nchans, nrows+2, ncols+2))
+    print(padded_images.shape)
+    for i in range(input_images.shape[0]):
+        padded_images[i, 0: ,1:nrows+1,1:ncols+1] = input_images[i]         
     return padded_images
 
 # Проверка происходит автоматически вызовом следующего кода
@@ -80,8 +89,14 @@ def get_padding2d(input_images):
 #  в коде для сдачи задания должно быть закомментировано):
 #print(torch.allclose(get_padding2d(input_images), correct_padded_images))
 
+#Calculated the shape of the input tensor after a convolution layer has been
+#applied
 def calc_out_shape(input_matrix_shape, out_channels, kernel_size, stride, padding):
-    out_shape = #TODO
+    nrows_in = input_matrix_shape[2]
+    ncols_in = input_matrix_shape[3]
+    nrows_out = (nrows_in+padding*2 - kernel_size)//stride+1
+    ncols_out = (ncols_in+padding*2- kernel_size)//stride+1
+    out_shape = [input_matrix_shape[0], out_channels, nrows_out, ncols_out]
     return out_shape
 
 out = calc_out_shape(input_matrix_shape=[2, 3, 10, 10],
@@ -95,3 +110,78 @@ out = calc_out_shape(input_matrix_shape=[2, 3, 10, 10],
 
 # ... и ещё несколько подобных кейсов
 
+
+# Abstract class for the convolution layer
+class ABCConv2d(ABC):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def set_kernel(self, kernel):
+        self.kernel = kernel
+
+    @abstractmethod
+    def __call__(self, input_tensor):
+        pass
+
+
+# Wrapper class for torch.nn.Conv2d to unify the interface
+class Conv2d(ABCConv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size,
+                                      stride, padding=0, bias=False)
+
+    def set_kernel(self, kernel):
+        self.conv2d.weight.data = kernel
+
+    def __call__(self, input_tensor):
+        return self.conv2d(input_tensor)
+
+# функция, создающая объект класса cls и возвращающая свертку от input_matrix
+def create_and_call_conv2d_layer(conv2d_layer_class, stride, kernel, input_matrix):
+    out_channels = kernel.shape[0]
+    in_channels = kernel.shape[1]
+    kernel_size = kernel.shape[2]
+
+    layer = conv2d_layer_class(in_channels, out_channels, kernel_size, stride)
+    layer.set_kernel(kernel)
+
+    return layer(input_matrix)
+
+
+# Функция, тестирующая класс conv2d_cls.
+# Возвращает True, если свертка совпадает со сверткой с помощью torch.nn.Conv2d.
+def test_conv2d_layer(conv2d_layer_class, batch_size=2,
+                      input_height=4, input_width=4, stride=2):
+    kernel = torch.tensor(
+                      [[[[0., 1, 0],
+                         [1,  2, 1],
+                         [0,  1, 0]],
+
+                        [[1, 2, 1],
+                         [0, 3, 3],
+                         [0, 1, 10]],
+
+                        [[10, 11, 12],
+                         [13, 14, 15],
+                         [16, 17, 18]]]])
+    in_channels = kernel.shape[1]
+
+    input_tensor = torch.arange(0, batch_size * in_channels *
+                                input_height * input_width,
+                                out=torch.FloatTensor()) \
+        .reshape(batch_size, in_channels, input_height, input_width)
+
+    custom_conv2d_out = create_and_call_conv2d_layer(
+        conv2d_layer_class, stride, kernel, input_tensor)
+    conv2d_out = create_and_call_conv2d_layer(
+        Conv2d, stride, kernel, input_tensor)
+       
+    #print(f" proper output: {conv2d_out}, shape {conv2d_out.shape}")
+
+    return torch.allclose(custom_conv2d_out, conv2d_out) \
+              and (custom_conv2d_out.shape == conv2d_out.shape)
+
+print(test_conv2d_layer(Conv2d))

4e720d8a6210cd3c183988113081ecedc34dcbc7
Maneka07 <maneka07@yandex.ru>
2022-02-02 15:19:15 +0300

    implemented batch normalization using moving averages

--- batch_norm.py
+++ batch_norm.py
@@ -8,25 +8,72 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-def custom_batch_norm1d(input_tensor, eps):
-    normed_tensor = # Напишите в этом месте нормирование входного тензора
-    return normed_tensor
+
+class CustomBatchNorm1d:
+    def __init__(self, weight, bias, eps, momentum):
+        self.weight = weight
+        self.bias = bias
+        self.eps = eps
+        self.momentum = momentum
+        self.train_mode = True
+        self.EMA_mean = torch.zeros(weight.shape[0])
+        self.EMA_var = torch.ones(weight.shape[0])
+
+    def __call__(self, input_tensor):
+        if self.train_mode:
+            var, mean = torch.var_mean(input_tensor, dim=0, unbiased=False)
+            normed_tensor = (input_tensor - mean)/(torch.sqrt(var + self.eps))
+            #update moving averages
+            n = input_tensor.shape[0]
+            self.EMA_mean = self.momentum*self.EMA_mean + (1-self.momentum)*mean
+            self.EMA_var = self.momentum*self.EMA_var + (1-self.momentum)*n*var/(n-1)
+        else:
+            normed_tensor = (input_tensor - self.EMA_mean)/torch.sqrt(self.EMA_var + self.eps)
+        normed_tensor = normed_tensor*self.weight + self.bias
+        return normed_tensor
+    
+    def eval(self):
+        self.train_mode = False
+
+# def custom_batch_norm1d(input_tensor, weight, bias, eps):
+    
+#     var, mean = torch.var_mean(input_tensor, dim=0, unbiased=False)
+#     normed_tensor = (input_tensor - mean)/(torch.sqrt(var + eps))
+#     normed_tensor = normed_tensor*weight + bias
+#     return normed_tensor
 
 
-input_tensor = torch.Tensor([[0.0, 0, 1, 0, 2], [0, 1, 1, 0, 10]])
-batch_norm = nn.BatchNorm1d(input_tensor.shape[1], affine=False)
+input_size = 7
+batch_size = 5
+input_tensor = torch.randn(batch_size, input_size, dtype=torch.float)
+eps = 1e-3
+batch_norm = nn.BatchNorm1d(input_size, eps=eps)
+batch_norm.bias.data = torch.randn(input_size, dtype=torch.float)
+batch_norm.weight.data = torch.randn(input_size, dtype=torch.float)
+batch_norm.momentum = 0.5
+
+custom_batch_norm1d = CustomBatchNorm1d(batch_norm.weight.data,
+                                        batch_norm.bias.data, eps, batch_norm.momentum)
 
 # Проверка происходит автоматически вызовом следующего кода
 # (раскомментируйте для самостоятельной проверки,
 #  в коде для сдачи задания должно быть закомментировано):
-# import numpy as np
-# all_correct = True
-# for eps_power in range(10):
-#     eps = np.power(10., -eps_power)
-#     batch_norm.eps = eps
-#     batch_norm_out = batch_norm(input_tensor)
-#     custom_batch_norm_out = custom_batch_norm1d(input_tensor, eps)
+all_correct = True
 
-#     all_correct &= torch.allclose(batch_norm_out, custom_batch_norm_out)
-#     all_correct &= batch_norm_out.shape == custom_batch_norm_out.shape
-# print(all_correct)
+for i in range(8):
+    torch_input = torch.randn(batch_size, input_size, dtype=torch.float)
+    norm_output = batch_norm(torch_input)
+    custom_output = custom_batch_norm1d(torch_input)
+    all_correct &= torch.allclose(norm_output, custom_output, atol=1e-06) \
+        and norm_output.shape == custom_output.shape
+
+batch_norm.eval()
+custom_batch_norm1d.eval()
+
+for i in range(8):
+    torch_input = torch.randn(batch_size, input_size, dtype=torch.float)
+    norm_output = batch_norm(torch_input)
+    custom_output = custom_batch_norm1d(torch_input)
+    all_correct &= torch.allclose(norm_output, custom_output, atol=1e-06) \
+        and norm_output.shape == custom_output.shape
+print(all_correct)
